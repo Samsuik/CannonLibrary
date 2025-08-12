e3bebf3680e9f74779a4d880802056789176a3ea
Samsuik <kfian294ma4@gmail.com>
2025-08-12 19:16:57 +0100

    a

--- examples/src/main/java/me/samsuik/cannonlib/examples/CannonRatioExample.java
+++ examples/src/main/java/me/samsuik/cannonlib/examples/CannonRatioExample.java
@@ -1,6 +1,7 @@
 package me.samsuik.cannonlib.examples;
 
 import me.samsuik.cannonlib.World;
+import me.samsuik.cannonlib.block.Blocks;
 import me.samsuik.cannonlib.entity.component.EntityComponents;
 import me.samsuik.cannonlib.entity.component.EntityConditions;
 import me.samsuik.cannonlib.entity.helpers.CannonRatio;
@@ -13,19 +14,18 @@ import java.util.List;
 public final class CannonRatioExample {
     private static final String RATIO = """
             // "1.2s 384 4os-osrb" by canabutter (https://discord.com/channels/778715854513635359/1373025771660251176/1373025771660251176) 
+            // modified to remove the osrb
             Sand Power      Amount: -    Tick: 0
               rev           Amount: 1    Tick: 9
               sand          Amount: 381  Tick: 10+
-            
+
             Hammer Power    Amount: -    Tick: 4
               hybrid sand   Amount: 1    Tick: 4-8
               hammer        Amount: 458  Tick: 10
               stopper       Amount: 13   Tick: 16
               os sand       Amount: 4    Tick: 16-17
               os hammer     Amount: 8    Tick: 18
-              osrb sand     Amount: 9    Tick: 18
-              osrb hammer   Amount: 14   Tick: 19
-              scatter       Amount: -    Tick: 24
+              scatter       Amount: 10   Tick: 24
             """;
     private static final Vec3i GUIDER = new Vec3i(0, 310, 0);
 
