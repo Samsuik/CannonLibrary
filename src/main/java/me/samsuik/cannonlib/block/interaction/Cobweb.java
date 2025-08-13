486eec75bc458e93b56374701d787e089a7eebf5
Samsuik <kfian294ma4@gmail.com>
2025-08-13 17:33:23 +0100

    use the correct stuck speed for cobwebs

--- src/main/java/me/samsuik/cannonlib/block/interaction/Cobweb.java
+++ src/main/java/me/samsuik/cannonlib/block/interaction/Cobweb.java
@@ -7,6 +7,6 @@ import me.samsuik.cannonlib.physics.vec3.Vec3d;
 public final class Cobweb implements Interaction {
     @Override
     public void touched(final Entity entity) {
-        entity.putData(EntityDataKeys.STUCK_SPEED, Vec3d.xyz(0.25f));
+        entity.putData(EntityDataKeys.STUCK_SPEED, new  Vec3d(0.25f, 0.05f, 0.25f));
     }
 }
