7a86d88ddfd964af0e29e24fbb134849896253c9
Samsuik <kfian294ma4@gmail.com>
2025-08-13 17:35:09 +0100

    Revert "use the correct ratio"
    This reverts commit d2ce1e8513bf25f90ba89f2d12c57cfb8e6841c1.

--- examples/src/main/java/me/samsuik/cannonlib/examples/OSRBClippingExample.java
+++ examples/src/main/java/me/samsuik/cannonlib/examples/OSRBClippingExample.java
@@ -14,18 +14,17 @@ import java.util.List;
 public final class OSRBClippingExample {
     private static final String RATIO = """
             // "0.7 384 osrb 1 above barrel" (https://discord.com/channels/778715854513635359/1404593768493219871)
-            Sand Power      Amount: -    Tick: 0
-              sand          Amount: 381  Tick: -
+            Sand Power      Amount: -    Tick: 0.0
+              sand          Amount: 381  Tick: 8.3
+              rev           Amount: 10   Tick: 7.1
             
-            Hammer Power    Amount: -    Tick: 4
-              hammer        Amount: 396  Tick: 4
-              hybrid sand   Amount: 1    Tick: 4 (rendered after power)
-              scatter       Amount: 4    Tick: 5
-              swing 1       Amount: 2    Tick: 6
-              swing 2       Amount: 57   Tick: 7
-              osrb sand     Amount: 9    Tick: 7
-              osrb hammer   Amount: 23   Tick: 8
-              scatter       Amount: -    Tick: 14
+            Hammer Power    Amount: -    Tick: 4.0
+              hammer        Amount: 561  Tick: 8.1
+              hybrid sand 1 Amount: 2    Tick: 6.3
+              hybrid sand 2 Amount: 1    Tick: 7.3
+              osrb sand     Amount: 9    Tick: 8.3
+              osrb hammer   Amount: 31   Tick: 9.1
+              scatter       Amount: 6    Tick: 15
             """;
     private static final Vec3i GUIDER = new Vec3i(0, 310, 0);
 
